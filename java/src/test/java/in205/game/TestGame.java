0421e5516dbc666de3c5cdbfde7e2b4a031a36e0
alien <user@localhost.localdomain>
2020-02-19 08:11:36 +0100

    Exercice 8

--- java/src/test/java/in205/game/TestGame.java
+++ java/src/test/java/in205/game/TestGame.java
@@ -1,5 +1,4 @@
 package in205.game;
-import in205.ships.*;
 
 
 /**
@@ -8,35 +7,9 @@ import in205.ships.*;
 public class TestGame {
 
     public static void main(String[] args) {
-        Board board = new Board("test AI game");
-        board.print();
-
-        
-        AbstractShip D = new Destroyer();
-        AbstractShip S1 = new Submarine();
-        AbstractShip S2 = new Submarine();
-        AbstractShip B = new Battleship();
-        AbstractShip C = new Carrier();
-
-        AbstractShip[] ships ={D,S1,S2,B,C};
-        Integer totalShipsNbr = 5;
-
-        BattleShipsAI ai = new BattleShipsAI(board, board);
-        ai.putShips(ships);
-
-        Integer destroyedShipsCount  = 0;
-        
-        int[] coords = new int[2];
-        while(destroyedShipsCount < totalShipsNbr ){
-            Hit hit = ai.sendHit(coords);
-            if(hit != Hit.MISS && hit != Hit.STIKE){
-                ++destroyedShipsCount;
-            }
-            System.out.println(coords);
-            System.out.println(hit);
-            board.print(); 
-            sleep(1000);
-        }
+        Game game = new Game();
+        game.init();
+        game.run();
 
     }
 
@@ -47,4 +20,6 @@ public class TestGame {
             e.printStackTrace();
         }
     }
+
+
 }
